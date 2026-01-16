efc82eb2d05c47c4726288a26d49cb105cd76652
sora-jang <sora.peppermint@gmail.com>
2026-01-17 05:42:34 +0900

    Delete aboutnine directory

--- aboutnine/index.js
+++
@@ -1,8 +0,0 @@
-const startBtn = document.getElementById("startBtn");
-
-if (startBtn) {
-  startBtn.addEventListener("click", () => {
-    // 사용자 클릭 제스처 유지
-    window.location.href = "./call.html";
-  });
-}
